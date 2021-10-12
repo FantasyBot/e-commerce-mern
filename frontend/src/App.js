fbb9e416a05105cb32991524b1108cfe8702e522
FantasyBot <usernamegithub@gmail.com>
2021-10-13 00:02:24 +0400

    add some styles and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <div>
-      This is an App component2
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 };
 
