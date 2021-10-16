850f0352260c788beb7f7283cfc9d9ff09c3041c
FantasyBot <usernamegithub@gmail.com>
2021-10-16 22:00:27 +0400

    fix async await problem / webpack @polyfill

--- frontend/src/store/actions/productActions.js
+++ frontend/src/store/actions/productActions.js
@@ -4,25 +4,15 @@ import {
 } from '../types';
 import axios from 'axios';
 
-export const listProducts = () => (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST });
-
-        //    const {data} = await axios.get('/api/products')  
-        //    still not working async/await
-        axios.get('/api/products')
-            .then((resp) => {
-                dispatch({
-                    type: PRODUCT_LIST_SUCCESS,
-                    payload: resp.data
-                })
-            })
-            .catch((error) => {
-                dispatch({
-                    type: PRODUCT_LIST_FAIL,
-                    payload: error.response && error.response.data.message ? error.response.data.message : error.message
-                })
-            })
+        
+        const { data } = await axios.get('api/products')
+        dispatch({
+            type: PRODUCT_LIST_SUCCESS,
+            payload: data,
+        })
     }
     catch (err) {
         dispatch({
@@ -33,27 +23,15 @@ export const listProducts = () => (dispatch) => {
 }
 
 
-
-
-export const listProductDetails = (id) => (dispatch) => {
+export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST });
 
-        //    const {data} = await axios.get('/api/products')  
-        //    still not working async/await
-        axios.get(`/api/products/${id}`)
-            .then((resp) => {
-                dispatch({
-                    type: PRODUCT_DETAILS_SUCCESS,
-                    payload: resp.data
-                })
-            })
-            .catch((error) => {
-                dispatch({
-                    type: PRODUCT_DETAILS_FAIL,
-                    payload: error.response && error.response.data.message ? error.response.data.message : error.message
-                })
-            })
+        const { data } = await axios.get(`/api/products/${id}`)
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
     }
     catch (err) {
         dispatch({
