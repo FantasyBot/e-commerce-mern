db6072276f417e6d119f7de7a837721ccf5f59a6
FantasyBot <usernamegithub@gmail.com>
2021-10-29 16:37:38 +0400

    add controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,28 @@
+import Product from '../models/productModel.js'
+import asyncHandler from 'express-async-handler'
+
+
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    //Error cousing
+    // res.status(401)
+    // throw new Error('Not Auth');
+    res.json(products);
+})
+
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
+
+export {
+    getProductById,
+    getProducts
+}
