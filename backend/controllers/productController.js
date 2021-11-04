c15d35ed47e2f279ec1981dd19150cb93b9db3a5
FantasyBot <usernamegithub@gmail.com>
2021-11-04 16:58:02 +0400

    Add User Authentication Endpoint

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,25 +1,39 @@
 import Product from '../models/productModel.js'
 // import asyncHandler from 'express-async-handler'
 
+// const getProducts = asyncHandler(async (req, res) => {
+//     const products = await Product.find({})
+//     res.json(products);
+// })
+// const getProductById = asyncHandler(async (req, res) => {
+//     const product = await Product.findById(req.params.id)
+//     if (product) {
+//         res.json(product)
+//     } else {
+//         res.status(404)
+//         throw new Error('Product not found')
+//     }
+// })
 
-const getProducts = (async (req, res) => {
-    const products = await Product.find({})
-    //Error cousing
-    // res.status(401)
-    // throw new Error('Not Auth');
-    res.json(products);
-})
+const getProducts = (req, res, next) => {
+    Product.find({})
+        .then((products) => {
+            return res.json(products);
+        })
+        .catch((err) => {
+            next(err);
+        })
+}
 
-const getProductById = (async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-})
-
+const getProductById = (req, res, next) => {
+    Product.findById(req.params.id)
+        .then((product) => {
+            return res.json(product)
+        })
+        .catch(() => {
+            next(err);
+        })
+}
 
 export {
     getProductById,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,9 @@ import express from 'express';
 const router = express.Router();
 import { getProducts, getProductById } from '../controllers/productController.js';
 
-router.get('/', getProducts)
-router.get('/:id', getProductById)
+// router.get('/', getProducts)
+// router.get('/:id', getProductById)
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
