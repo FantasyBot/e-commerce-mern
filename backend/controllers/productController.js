2521fa4e9aa6bbd8f469bdc49f3f7e529274679a
FantasyBot <usernamegithub@gmail.com>
2021-11-30 18:44:49 +0400

    Create & Update Product Endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -28,7 +28,7 @@ const getProductById = async (req, res, next) => {
 
 // Delete a product
 // DELETE/api/products/:id
-// Private/Admin API
+// Private/Admin
 const deleteProduct = async (req, res, next) => {
     try {
         const product = await Product.findById(req.params.id);
@@ -41,9 +41,67 @@ const deleteProduct = async (req, res, next) => {
     }
 }
 
+// Create a product
+// POST/api/products
+// Private/Admin
+const createProduct = async (req, res, next) => {
+    try {
+        const product = await new Product({
+            name: 'Sample name',
+            price: 0,
+            user: req.user._id,
+            image: '/images/sample.jpg',
+            brand: 'Sample brand',
+            category: 'Sample category',
+            countInStock: 0,
+            numReviews: 0,
+            description: 'Sample description',
+        })
+        const createdProduct = await product.save();
+        res.status(201).json(createdProduct);
+    } catch (err) {
+        res.status(404);
+        return next(err);
+    }
+}
+
+//  Update a product
+//  PUT /api/products/:id
+//  Private/Admin
+const updateProduct = async (req, res, next) => {
+    const {
+        name,
+        price,
+        description,
+        image,
+        brand,
+        category,
+        countInStock,
+    } = req.body
+
+    try {
+        const product = await Product.findById(req.params.id)
+        
+        product.name = name
+        product.price = price
+        product.description = description
+        product.image = image
+        product.brand = brand
+        product.category = category
+        product.countInStock = countInStock
+
+        const updatedProduct = await product.save()
+        res.json(updatedProduct)
+    } catch (err) {
+        res.status(404);
+        return next("Product not found");
+    }
+}
 
 export {
     getProductById,
     getProducts,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js';
+import { getProducts, getProductById, deleteProduct, createProduct, updateProduct } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
 router.delete('/:id', protect, admin, deleteProduct);
+router.post('/', protect, admin, createProduct);
+router.put('/:id', protect, admin, updateProduct);
 
 export default router;
